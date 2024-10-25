5860a5a5b482090891731e27c77c86e86ebc5551
Esbreon <tristan@boostix.nl>
2024-10-25 10:34:47 +0200

    Update PreWonenHerinnering.py

--- PreWonenHerinnering.py
+++ PreWonenHerinnering.py
@@ -10,7 +10,7 @@ AIRTABLE_TABLE_NAME = os.environ.get('AIRTABLE_TABLE_NAME')  # Bijvoorbeeld: "Pr
 AIRTABLE_API_KEY = os.environ.get('AIRTABLE_API_KEY')
 
 # WhatsApp template configuratie
-WHATSAPP_TEMPLATE_ID = os.environ.get('WHATSAPP_TEMPLATE_ID_PWG_TEST')
+WHATSAPP_TEMPLATE_ID = os.environ.get('WHATSAPP_TEMPLATE_ID_G_TEST')
 
 def get_airtable_data():
     """Haalt data op uit Airtable."""
