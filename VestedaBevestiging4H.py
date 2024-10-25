fc1832e352174aa0bb034811ed6f611c89fdf1ea
Esbreon <tristan@boostix.nl>
2024-10-25 10:47:45 +0200

    Update VestedaBevestiging4H.py

--- VestedaBevestiging4H.py
+++ VestedaBevestiging4H.py
@@ -6,7 +6,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 # Airtable configuration
 AIRTABLE_BASE_ID = os.environ.get('AIRTABLE_BASE_ID')
-AIRTABLE_TABLE_NAME = os.environ.get('AIRTABLE_TABLE_NAME')
+AIRTABLE_TABLE_NAME = os.environ.get('AIRTABLE_V4H')
 AIRTABLE_API_KEY = os.environ.get('AIRTABLE_API_KEY')
 WHATSAPP_TEMPLATE_ID = os.environ.get('WHATSAPP_TEMPLATE_ID_VESTEDA_4H')
 
