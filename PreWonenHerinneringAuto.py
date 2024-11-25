8c65563422d324784ae9efd94112fedb7fb0f263
Esbreon <tristan@boostix.nl>
2024-11-25 14:33:08 +0100

    Update PreWonenHerinneringAuto.py

--- PreWonenHerinneringAuto.py
+++ PreWonenHerinneringAuto.py
@@ -4,9 +4,6 @@ import requests
 import pandas as pd
 from datetime import datetime
 import msal
-from apscheduler.schedulers.blocking import BlockingScheduler
-from apscheduler.triggers.cron import CronTrigger
-import pytz
 
 class OutlookClient:
     def __init__(self):
@@ -339,7 +336,6 @@ def process_data():
     
     try:
         outlook = OutlookClient()
-        
         try:
             excel_file = outlook.download_excel_attachment(
                 sender_email=os.environ.get('SENDER_EMAIL'),
@@ -350,7 +346,6 @@ def process_data():
                 try:
                     process_excel_file(excel_file)
                 finally:
-                    # Always try to clean up the file
                     if os.path.exists(excel_file):
                         print(f"\nVerwijderen tijdelijk bestand: {excel_file}")
                         os.remove(excel_file)
@@ -363,18 +358,5 @@ def process_data():
     except Exception as e:
         print(f"Algemene fout: {str(e)}")
 
-# Scheduler configuration
-def main():
-    scheduler = BlockingScheduler(timezone=pytz.timezone('Europe/Amsterdam'))
-    
-    # Schedule process_data to run at 17:30 every weekday (Mon-Fri)
-    scheduler.add_job(process_data, CronTrigger(day_of_week='mon-fri', hour=14, minute=00))
-    
-    print("\nScheduler gestart, wacht op geplande taken...")
-    try:
-        scheduler.start()
-    except (KeyboardInterrupt, SystemExit):
-        print("\nScheduler gestopt.")
-
 if __name__ == "__main__":
-    main()
+    process_data()
